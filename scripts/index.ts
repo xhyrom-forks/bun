659d904f1cdfc6017cdfd0e8548a91e77175989f
xHyroM <generalkubo@gmail.com>
2022-08-28 20:10:41 +0200

    feat(script): add comment

--- scripts/index.ts
+++ scripts/index.ts
@@ -12,11 +12,24 @@ for (const issue of issues) {
     if (issue.user.login !== 'xHyroM') continue;
 
     const body = parse(issue.body);
-    const content = `- [${body.branch}](https://github.com/xhyrom-forks/bun/tree/${body.branch}) ${body.pr ? `| [${body.pr}](https://github.com/oven-sh/bun/pull/${body.pr})` : ''}\n`;
+    const content = `- [${body.branch}](https://github.com/xhyrom-forks/bun/tree/${body.branch}) ${body.pr ? `| [#${body.pr}](https://github.com/oven-sh/bun/pull/${body.pr})` : ''}\n`;
     if (issue.state === 'open') {
         RO += content;
     } else {
         INP += content;
+
+        await fetch(`https://api.github.com/repos/xhyrom-forks/bun/issues/${issue.number}/comments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.github+json',
+                'User-Agent': 'xhyrom-forks-bun-global_info',
+                'Authorization': `token ${process.env.GITHUB_TOKEN}`
+            },
+            body: JSON.stringify({
+                body: `Closes after [#${body.pr}](https://github.com/oven-sh/bun/pull/${body.pr})`
+            })
+        })
     }
 }
 
